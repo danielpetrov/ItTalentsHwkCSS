62627c97fb6d78dd5018b1ab53e94d1e90650901
daniel <danielpetrov222@gmail.com>
2015-12-25 10:22:14 +0200

    To task 5

--- Layout/switch-between-styles.js
+++ Layout/switch-between-styles.js
@@ -1,3 +1,15 @@
-/**
- * Created by daniel on 23.12.2015 г..
- */
+function switch_style ( css_title )
+{
+
+    var i, link_tag ;
+    for (i = 0, link_tag = document.getElementsByTagName("link") ;
+         i < link_tag.length ; i++ ) {
+        if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
+            link_tag[i].title) {
+            link_tag[i].disabled = true ;
+            if (link_tag[i].title == css_title) {
+                link_tag[i].disabled = false ;
+            }
+        }
+    }
+}
